1e8257ebf3a5ac3c119cb5b8c3e311a09d786dfa
ajaber82 <ajaber82@gmail.com>
2018-12-06 21:44:42 -0600

    start getting great with templating

--- part-shop-web/src/main/java/com/partsshop/web/PartsShopWebApplication.java
+++ part-shop-web/src/main/java/com/partsshop/web/PartsShopWebApplication.java
@@ -4,7 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.partsshop.web"}) 
@@ -15,4 +19,7 @@ public class PartsShopWebApplication {
 		LOG.info("Application Server Start");
 		SpringApplication.run(PartsShopWebApplication.class, args);
 	}
+	
+	
+	
 }
